b7e02237a175881c04f7369bf43f512c5ebb7a4e
caballerojose597 <caballerojose597@gmail.com>
2020-03-16 10:56:12 -0600

    Agregando segundo metodo al HeapSort

--- src/edu/ujcv/progra1/HeapSort.java
+++ src/edu/ujcv/progra1/HeapSort.java
@@ -17,9 +17,7 @@ public class HeapSort implements SortTester {
         for (int i = n / 2 - 1; i >= 0; i--)
             heapify(array, n, i);
 
-
         for (int i = n - 1; i >= 0; i--) {
-
             int temp = array[0];
             array[0] = array[i];
             array[i] = temp;
@@ -32,8 +30,23 @@ public class HeapSort implements SortTester {
         int largest = i;
         int l = 2 * i + 1;
         int r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
     }
 }
+
+
 
 
 
